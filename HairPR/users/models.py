ac41160c24c8c81a3956249f42dbe82c62825765
Michael Yuan <michaelyuan1@yahoo.com>
2012-02-26 14:52:09 -0500

    added in registration and admin site

--- HairPR/users/models.py
+++ HairPR/users/models.py
@@ -4,3 +4,6 @@ from django.core.validators import validate_email
 
 class User(models.Model):
 	email = models.CharField(max_length=200, validators=[validate_email])
+
+	def __unicode__(self):
+		return self.email
