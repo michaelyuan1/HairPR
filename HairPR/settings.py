bd667b90560db63be09f4f96e183ae0242bba74a
Michael Yuan <michaelyuan1@yahoo.com>
2012-02-26 15:11:20 -0500

    stuff work??? and email?

--- HairPR/settings.py
+++ HairPR/settings.py
@@ -6,6 +6,12 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'buhbuhbuhbuhbuhbuhbowser@gmail.com'
+EMAIL_HOST_PASSWORD = 'michael1'
+EMAIL_PORT = 587
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
--- HairPR/users/views.py
+++ HairPR/users/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from users.models import User
+from django.core.mail import send_mail
+
 
 def home(request):
 	return render_to_response('users/home.html', context_instance=RequestContext(request))
@@ -10,4 +12,5 @@ def home(request):
 def submitEmail(request):
 	newUser = User(email=request.POST['email'])
 	newUser.save();
+	send_mail('Thanks For Signing Up for HairPR!', 'Thank You!', 'mytwo@princeton.edu', [request.POST['email']], fail_silently=False)
 	return render_to_response('users/thankyou.html', context_instance=RequestContext(request))
